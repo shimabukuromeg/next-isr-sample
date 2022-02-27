139b60f6a9f7cf87550f16f2fae36a83b18f21f0
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2022-02-27 16:16:18 +0900

    ビルド時に現在時刻を渡すようにした

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,14 @@
 import type { AppProps } from 'next/app';
+import { createTheme, NextUIProvider } from '@nextui-org/react';
+
+const darkTheme = createTheme({
+  type: 'dark',
+});
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
+  <NextUIProvider theme={darkTheme}>
+    <Component {...pageProps} />
+  </NextUIProvider>
 );
 
 export default MyApp;
